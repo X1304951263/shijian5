bf8e6b2661f7f7586771ba112ea3658d3e27a4db
1304951263@qq.com <1304951263@qq.com>
2020-10-10 19:39:35 +0800

    实践5

---
+++ app/src/main/java/com/example/drawable/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.drawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
